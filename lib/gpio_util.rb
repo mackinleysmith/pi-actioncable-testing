c12db71ea9ac4a1193ef9d4dfec1893df144306b
MacKinley Smith <smit1625@msu.edu>
2016-07-29 18:56:44 -0600

    Dedicated read method

--- lib/gpio_util.rb
+++ lib/gpio_util.rb
@@ -6,17 +6,30 @@ class GpioUtil
   end
 
   def write(value:)
-    command_pin! pin, value
+    command_pin! value
   end
 
   def read(mode: 'read')
-    command_pin! pin, "-d in -m down -a #{mode}"
+    return read_from_pin if mode == 'read'
+    command_pin! "-d in -m down -a #{mode}"
+    yield if block_given?
+  end
+
+  protected
+  attr_reader :pin
+
+  def command_pin!(command)
+    Kernel.system formulate_command command
+  end
+
+  def read_from_pin
+    `#{formulate_command '-d in'}`
   end
 
   private
-  attr_reader :pin
 
-  def command_pin!(pin, command)
-    Kernel.system "#{BIN_PATH} -p #{pin} #{command}"
+  def formulate_command(command)
+    "#{BIN_PATH} -p #{pin} #{command}"
   end
+
 end
